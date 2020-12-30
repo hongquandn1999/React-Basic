c0b55b2176b473692e1ae86285e45be8c309bfec
hhq85 <hhq85@gmail.com>
2020-12-30 20:15:45 +0700

    State

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import './App.css';
 import Person from './persons/person';
 
 class App extends Component {
+	state = {
+		persons: [
+			{ name: 'Noelle', anime: 'Black Clover' },
+			{ name: 'Nezuko', anime: 'Kimetsu no yaiba' },
+			{ name: 'Hinata', anime: 'Naruto' },
+		],
+	};
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<Person name="Noelle" movie="Black Clover" />
-					<Person name="Nezuko" movie="Kimetsu no yaiba">
+					<Person
+						name={this.state.persons[0].name}
+						movie={this.state.persons[0].anime}
+					/>
+					<Person
+						name={this.state.persons[1].name}
+						movie={this.state.persons[1].anime}
+					>
 						This movie used to appear at movie theater
 					</Person>
-					<Person name="Hinata" movie="Naruto" />
+					<Person
+						name={this.state.persons[2].name}
+						movie={this.state.persons[2].anime}
+					/>
 					<p>
 						Edit <code>src/App.js</code> and save to reload.
 					</p>
